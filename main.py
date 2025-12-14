cab24bfe99656ece65a2b6f3fb1272dc78c0c282
holpunk <aqshey@gmail.com>
2025-12-14 22:19:16 +0530

    setting up sample docker build and test stages

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+items = []
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    return jsonify({'items': items})
+
+@app.route('/items', methods=['POST'])
+def add_item():
+    data = request.get_json()
+    items.append(data)
+    return jsonify({'message': 'Item added', 'item': data}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
